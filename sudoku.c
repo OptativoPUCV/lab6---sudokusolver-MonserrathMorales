3a5dfe83ff166f013a334158f03dc9572f0f04d5
Replit user <>
2023-10-04 23:46:01 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "list.h"
 
 
@@ -48,19 +49,49 @@ int is_valid(Node* n){
     return 1;
 }
 
-
+/* > - Utilice la función Node* copy(Node* n) para copiar nodos. */
 List* get_adj_nodes(Node* n){
-  List* list=createList();
+  List* list=createList();/*
+  int numero = 1;
+  bool existe = false;
 
   for(size_t i = 0; i < 9; i++) {
     for(size_t k = 0; k < 9; k++) {
-      // si esta vacio, crear un posible movimiento (nodo adyacente)
+      // si esta vacio, crear una posible jugada (nodo adyacente). Se debe copiar el nodo n.
       if(n->sudo[i][k] == ' ') {
+        // Node * posibleJugada = (Node *) malloc(sizeof(Node));
+        Node * posibleJugada = copy(n);
+
+        // rellenar la casilla con un valor del 1 al 9, solo si cumple con las reglas del sudoku (que no se repita el numero en la fila, columna o cuadrado [3][3])
+
+        // condicion 1: que el numero no este en la misma fila
+        while(k < 9) {
+          if(posibleJugada->sudo[i][k] == numero) {
+            
+          }
+          k++;
+        }
+
+        // condicion 2: que el numero no este en la misma columna
+        posibleJugada->sudo[i][k] = numero;
+
+
         
+        // condicion 3: que el numero no este en el mismo cuadrado (3x3)
+        
+
+        
+        posibleJugada->sudo[i][k] = numero;
+
+        pushBack(list, posibleJugada);
       }
     }
   }
-  
+
+
+
+
+  */
   return list;
 }
 
@@ -70,6 +101,20 @@ int is_final(Node* n){
 }
 
 Node* DFS(Node* initial, int* cont){
+  Stack* pila = createStack();
+  push(pila,initial);
+  
+  while (size(pila) != 0){
+     Node * nodo = pop(pila);
+    
+     List * adj = get_adj_nodes(nodo);
+     Node * aux = first(adj);
+    
+     while(aux){
+        push(pila,aux);
+        aux=next(adj);
+     }
+
   return NULL;
 }
 
