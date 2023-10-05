09181ce00630a749cc6c81cb81abde51c0f81b60
Replit user <>
2023-10-05 19:38:56 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -58,7 +58,6 @@ int is_valid(Node* n){
           }
 
           // condicion 2: que el numero no este en la misma columna
-          size_t columna = k;
           for (size_t columna = 0; columna < 9; columna++) {
             if (columna != k && n->sudo[i][columna] == valor) {
               return 0;
@@ -69,7 +68,7 @@ int is_valid(Node* n){
           size_t col_inicial = (k / 3) * 3;
           for(size_t fila = fila_inicial; fila < fila_inicial + 3; fila++) {
             for(size_t columna = col_inicial; columna < col_inicial + 3; columna++) {
-              if((fila != i) || (columna != k) && n->sudo[fila][columna] == valor) {
+              if((fila != i) || (columna != k) && (n->sudo[fila][columna] == valor)) {
                 return 0;
               }
             }
